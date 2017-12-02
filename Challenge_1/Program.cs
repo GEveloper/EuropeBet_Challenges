d60f7fe3f0c95e768246c06d6a88ee2f0617d335
GEveloper <GEveloper@users.noreply.github.com>
2017-12-02 20:20:19 +0400

    Implement create_coffee and drink_coffee methods.

--- Challenge_1/Program.cs
+++ Challenge_1/Program.cs
@@ -8,6 +8,7 @@ namespace Challenge_1
     class Program
     {
         static bool alive = true;
+        static int drank_coffee = 0;
 
         static void Main(string[] args)
         {
@@ -18,12 +19,32 @@ namespace Challenge_1
             }
         }
 
-        private static void code()
+        private static void drink_coffee()
         {
-            throw new NotImplementedException();
+            int daily_limit = 3 * create_coffee();
+
+            if (drank_coffee > daily_limit)
+            {
+                Console.WriteLine("Stop drinking coffee!");
+                return;
+            }
+
+            drank_coffee += create_coffee();
+            Console.WriteLine("A programmer is just a tool that converts caffeine into code!");
         }
 
-        private static void drink_coffee()
+        private static int create_coffee()
+        {
+            int coffee = 1;
+            int suggar = 2;
+            int water = 400;
+
+            Console.WriteLine("Coffee created.");
+
+            return coffee + suggar + water;
+        }
+
+        private static void code()
         {
             throw new NotImplementedException();
         }
