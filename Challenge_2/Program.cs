7bd6f3400958d76e84626f6c13c8427cb6fe76f0
GEveloper <GEveloper@users.noreply.github.com>
2017-12-02 23:45:04 +0400

    Add try/catch block with custom exception handling. Set console background and foreground colors.

--- Challenge_2/Program.cs
+++ Challenge_2/Program.cs
@@ -9,33 +9,49 @@ namespace Challenge_2
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+
             Coder you = new Coder(Name: "GEveloper", ExperienceYears: 4);
 
-            while (you.alive)
+            try
             {
-                you.drink_coffee();
-                you.code(); // actually turning coffee in to code))
+                while (you.alive)
+                {
+                    you.drink_coffee();
+                    you.code(); // actually turning coffee in to code))
 
-                if (you.are_stressed)
-                {
-                    you.go_rest();
-                    you.go_have_fun();
-                    you.go_sleep();
+                    if (you.are_stressed)
+                    {
+                        if (!you.go_rest())
+                        {
+                            break;
+                        }
+
+                        you.go_have_fun();
+                        you.go_sleep();
+                    }
+
+                    if (you.are_alone) // and you are, because you are coder
+                    {
+                        you.go_alive();
+                        you.go_find_soulmate();
+                        you.cheer_up();
+                    }
                 }
+
+                bool result = you.are_saint ? you.go_to_heaven() : you.go_to_hell();
                 
-                if (you.are_alone) // and you are, because you are coder
-                {
-                    you.go_alive();
-                    you.go_find_soulmate();
-                    you.cheer_up();
-                }
+                /* exception is thrown here,
+                 * you are coder, you know everything is lie, matrix doesn't work that way)) */
+                throw new FuckThatLifeException("Everything Is Lie!");
+            }
+            catch (FuckThatLifeException fex)
+            {
+                Console.WriteLine(string.Format("<< {0} >>", fex.Message));
             }
 
-            bool result = you.are_saint ? you.go_to_heaven() : you.go_to_hell();
-
-            /* exception is thrown here,
-             * you are coder, you know everything is lie, matrix doesn't work that way)) */
-
+            Console.ReadKey();
         }
     }
 }
