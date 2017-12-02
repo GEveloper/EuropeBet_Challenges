6cb4ff3a8b0aedc3ef3c340e5971a9ffb96f1bfe
GEveloper <GEveloper@users.noreply.github.com>
2017-12-02 20:10:18 +0400

    Declare necessary functions.

--- Challenge_1/Program.cs
+++ Challenge_1/Program.cs
@@ -7,13 +7,25 @@ namespace Challenge_1
 {
     class Program
     {
+        static bool alive = true;
+
         static void Main(string[] args)
         {
-            while(alive)
+            while (alive) 
             {
                 drink_coffee();
                 code();
             }
+        }
+
+        private static void code()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void drink_coffee()
+        {
+            throw new NotImplementedException();
         }
     }
 }
