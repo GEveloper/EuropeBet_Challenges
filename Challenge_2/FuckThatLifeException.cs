a75a0fb207feb2b967fa2005e6e3bf9fdc77f577
GEveloper <GEveloper@users.noreply.github.com>
2017-12-02 23:43:30 +0400

    Add FuckThatLifeException class.

---
+++ Challenge_2/FuckThatLifeException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Challenge_2
+{
+    class FuckThatLifeException : Exception
+    {
+        public FuckThatLifeException(string message) : base(message)
+        {
+            // do nothing
+        }
+    }
+}
