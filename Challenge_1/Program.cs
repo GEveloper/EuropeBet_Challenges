c5d08225e81a9d37d0fe1a823800b21212bb4a71
GEveloper <GEveloper@users.noreply.github.com>
2017-12-02 20:33:04 +0400

    Implement code method, add some console messages and increase coffee daily limit.

--- Challenge_1/Program.cs
+++ Challenge_1/Program.cs
@@ -17,20 +17,25 @@ namespace Challenge_1
                 drink_coffee();
                 code();
             }
+
+            Console.WriteLine("Ohh, It's zombie! :O");
+            Console.ReadKey();
         }
 
         private static void drink_coffee()
         {
-            int daily_limit = 3 * create_coffee();
+            int daily_limit = 5 * create_coffee();
 
-            if (drank_coffee > daily_limit)
+            if (drank_coffee >= daily_limit)
             {
-                Console.WriteLine("Stop drinking coffee!");
+                Console.WriteLine("Warning: Stop drinking coffee! or die." + Environment.NewLine);
+                alive = false;
                 return;
             }
 
             drank_coffee += create_coffee();
-            Console.WriteLine("A programmer is just a tool that converts caffeine into code!");
+            Console.WriteLine("// Nice coffee! A programmer is just a tool " +
+                "that converts caffeine into code!");
         }
 
         private static int create_coffee()
@@ -38,15 +43,17 @@ namespace Challenge_1
             int coffee = 1;
             int suggar = 2;
             int water = 400;
-
-            Console.WriteLine("Coffee created.");
-
+            
             return coffee + suggar + water;
         }
 
         private static void code()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(
+                "static void Main(string[] args)" + Environment.NewLine +
+                "{" + Environment.NewLine +
+                    "\tConsole.WriteLine(\"Hello World!\");" + Environment.NewLine +
+                "}" + Environment.NewLine + Environment.NewLine);
         }
     }
 }
