fa7c3eba7454b35c7f2b4a52831309f1da9cecf3
GEveloper <GEveloper@users.noreply.github.com>
2017-12-02 21:02:31 +0400

    Add incorrect code for challenge #2.

---
+++ Challenge_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Challenge_2
+{
+    class Life
+    {
+        static void Main(string[] args)
+        {
+            var you = new Coder();
+            while(you.alive)
+            {
+                you.drink_coffee();
+                you.code(); //actualy turning coffee in to code))
+
+                if(you.are_stressed)
+                {
+                    you.go_rest();
+                    you.go_have_fun();
+                    you.go_sleep();
+                }
+
+                if(you.are_alone) //and you are, because you are coder
+                {
+                    you.go_alive();
+                    you.go_find_soulmate();
+                    you.cheer_up();
+                }
+            }
+
+            you.are_saint?you.go_to_heaven():you.go_to_hell();
+            /*exception is thrown here,
+              you are coder you now everything is lie, matrix doesn't work that way)) */
+        }
+    }
+}
